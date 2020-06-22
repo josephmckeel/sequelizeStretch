d4f511c78e16f5a7389d6d20190c9b3d48ef497a
Taylor McKeel <josephmckeel1993@gmail.com>
2020-06-22 17:56:02 -0500

    seq

--- recipe.js
+++ recipe.js
@@ -1,7 +1,32 @@
-const db = require('./dbConfig');
-const Sequelize = require('sequelize');
-
+const db = require("./dbConfig");
+const Sequelize = require("sequelize");
+const { STRING, NUMBER, BOOLEAN, ENUM } = require("sequelize");
 //YOUR CODE GOES HERE
+
+// const seed = async (force = false) => {
+//   try {
+//     await db.sync(force);
+//   }
+// };
+
+const Recipe = db.define("recipe", {
+  name: {
+    type: STRING,
+    defaultValue: "cereal",
+    allowNull: false,
+  },
+  cookTime: {
+    type: NUMBER,
+    minValue: 1,
+    maxValue: 60,
+  },
+  vegan: {
+    type: BOOLEAN,
+  },
+  foodGroup: {
+    type: ENUM(["vegetable", "meat", "dairy", "grain", "fruit", "coffee"]),
+  },
+});
 
 //--------------------
 module.exports = Recipe;
